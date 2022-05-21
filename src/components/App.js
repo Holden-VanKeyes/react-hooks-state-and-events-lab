7eb91582cc2b001988b3dc7e67952bc44448e75e
Abram Shook <ajshook77@gmail.com>
2022-05-20 19:02:40 -0500

    commit tree bcc3e0b84f28e6ad7bb762f69b6df6b7ffc4f451

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,29 @@
-import React from "react";
-import ShoppingList from "./ShoppingList";
-import itemData from "../data/items";
+import React, { useState } from 'react'
+import ShoppingList from './ShoppingList'
+
+import itemData from '../data/items'
+//import { useState } from 'react/cjs/react.production.min'
 
 function App() {
+  const [toggle, setToggle] = useState('false')
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  function handleClick() {
+    setToggle(!toggle)
+  }
+
+  const appClass = toggle ? 'App dark' : 'App light'
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>
+          {toggle ? 'Dark Mode' : 'Light Mode'}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from 'react'
 
 function Item({ name, category }) {
+  const [cartUpdater, setCartUpdater] = useState(false)
+
+  function handleClick() {
+    setCartUpdater((cartUpdater) => !cartUpdater)
+  }
   return (
-    <li className="">
+    <li className={cartUpdater ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">
+        {cartUpdater ? 'Remove From' : 'Add To'} Cart
+      </button>
     </li>
-  );
+  )
 }
 
-export default Item;
+export default Item
